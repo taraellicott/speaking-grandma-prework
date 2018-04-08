52ffc16e55607abf986f0f8f4eafeeed786a0c2f
Tara Ellicott <taraellicott@gmail.com>
2018-04-08 19:11:05 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,12 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase != phrase.upcase()
- "HUH?! SPEAK UP, SONNY!"
+    "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
-   "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase = phrase.upcase()
     "NO, NOT SINCE 1938!"
-   
-end
+  end
 end
 
 
