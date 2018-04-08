2d8a134e575af2b7d77297b2bba6b350b30b702a
Tara Ellicott <taraellicott@gmail.com>
2018-04-08 19:07:03 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,11 +2,11 @@
 def speak_to_grandma(phrase)
  
   if phrase == "I LOVE YOU GRANDMA!"
-  "I LOVE YOU TOO PUMPKIN!"
+  return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase = phrase.upcase()
-   "NO, NOT SINCE 1938!"
+   return "NO, NOT SINCE 1938!"
    else
-     "HUH?! SPEAK UP, SONNY!"
+     return "HUH?! SPEAK UP, SONNY!"
 end
 
 
